9a93b187d46c3b54b3941ab4a9461964c0621188
iikonmyv <igorcostabr@gmail.com>
2022-10-08 21:25:12 -0300

    Segundo commit

--- app/src/main/java/br/com/example/introducao_android_studio/MainActivity.kt
+++ app/src/main/java/br/com/example/introducao_android_studio/MainActivity.kt
@@ -2,10 +2,15 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+        tvResultado.text = "Texto alterado"
     }
 }
