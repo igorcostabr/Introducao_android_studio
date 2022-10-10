877ca94348bdeae69410920436b303491c131dbb
iikonmyv <igorcostabr@gmail.com>
2022-10-10 14:20:34 -0300

    Navigation Drawer

---
+++ app/src/main/java/br/com/example/introducao_android_studio/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package br.com.example.introducao_android_studio.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
